013c235d018157ee1fbd3a94587476eb40ad638c
WangWenBo <wangwenbomx@gmail.com>
2014-09-30 22:45:37 +0800

    Update

---
+++ FPS/Enemy.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	//Transform组件
+	Transform m_transform;
+	//动画组件
+	Animator m_ani;
+	//角色旋转速度
+	float m_rotSpeed = 120;
+	//计时器
+	float m_timer = 2;
+	//生命值
+	//int m_life = 15;
+	//主角
+	Player m_player;
+	//寻路组件
+	NavMeshAgent m_agent;
+	//移动速度
+	float m_movSpeed = 0.15f;
+
+	void Start(){
+		m_transform = this.transform;
+		m_ani = this.GetComponent<Animator> ();
+		m_player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+		m_agent = GetComponent<NavMeshAgent> ();
+		//设置寻路目标
+		m_agent.SetDestination (m_player.transform.position);
+	}
+
+	void Update(){
+		if(m_player.m_life <= 0)
+			return;
+		
+		//获取当前动画状态
+		AnimatorStateInfo stateInfo = m_ani.GetCurrentAnimatorStateInfo (0);
+		
+		//如果处于待机状态
+		if(stateInfo.nameHash == Animator.StringToHash("Base Layer.idle")&&!m_ani.IsInTransition(0))
+		{
+			m_ani.SetBool("idle",false);
+			//待机一定时间
+			m_timer -= Time.deltaTime;
+			if(m_timer>0)
+				return;
+			//如果距离主角小于1.5m，进入攻击动画状态
+			if(Vector3.Distance(m_transform.position,m_player.m_transform.position)<1.5f)
+			{
+				m_ani.SetBool("attack",true);
+			}
+			else
+			{
+				//重置定时器
+				m_timer = 1;
+				//设置寻路目标点
+				m_agent.SetDestination (m_player.transform.position);
+				//进入跑步动画状态
+				m_ani.SetBool("run",true);
+			}
+		}
+		//如果处于跑步状态
+		if(stateInfo.nameHash == Animator.StringToHash("Base Layer.run")&&!m_ani.IsInTransition(0))
+		{
+			m_ani.SetBool("run",false);
+			//每隔一秒定位主角位置
+			m_timer -= Time.deltaTime;
+			if(m_timer<0)
+			{
+				m_agent.SetDestination (m_player.transform.position);
+				m_timer = 1;
+			}
+			//追向主角
+			MoveTo();
+			//如果距离主角小于1.5m，进入攻击动画
+			if(Vector3.Distance(m_transform.position,m_player.m_transform.position)<1.5f)
+			{
+				//停止寻路
+				m_agent.ResetPath();
+
+				m_ani.SetBool("attack",true);
+			}
+		}
+		//如果处于攻击状态
+		if(stateInfo.nameHash == Animator.StringToHash("Base Layer.attack")&&!m_ani.IsInTransition(0))
+		{
+			//面向主角
+			RotateTo();
+			m_ani.SetBool("attack",false);
+			//如果动画播完，重新进入待机状态
+			if(stateInfo.normalizedTime>=1.0f)
+			{
+				m_ani.SetBool("idle",true);
+				m_timer = 2;
+			}
+		}
+	}
+
+	void MoveTo(){
+		float speed = m_movSpeed * Time.deltaTime;
+		m_agent.Move (m_transform.TransformDirection ((new Vector3 (0, 0, speed))));
+	}
+
+	void RotateTo(){
+		//当前角度
+		Vector3 oldangle = m_transform.eulerAngles;
+		//获得面向主角的速度
+		m_transform.LookAt (m_player.m_transform);
+		float target = m_transform.eulerAngles.y;
+		//转向主角
+		float speed = m_rotSpeed * Time.deltaTime;
+		float angle = Mathf.MoveTowardsAngle (oldangle.y, target, speed);
+		m_transform.eulerAngles = new Vector3 (0, angle, 0);
+	}
+}
---
+++ FPS/Player.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	//组件
+	public Transform m_transform;
+	CharacterController m_ch;
+	Transform m_camTransform;
+
+	//摄像机旋转角度
+	Vector3 m_camRot;
+	float m_camHeight = 1.4f;
+
+	//角色移动速度
+	float m_movSpeed = 3.0f;
+
+	//重力
+	float m_gravity = 2.0f;
+
+	//生命值
+	public int m_life = 5;
+
+	void Start(){
+		m_transform = this.transform;
+		m_ch = this.GetComponent<CharacterController> ();
+
+		//获取摄像机
+		m_camTransform = Camera.main.transform;
+		Vector3 pos = m_transform.position;
+		pos.y += m_camHeight;
+		m_camTransform.position = pos;
+		m_camTransform.rotation = m_transform.rotation;
+		m_camRot = m_camTransform.eulerAngles;
+
+		Screen.lockCursor = true;
+	}
+
+	void Update(){
+		if (m_life <= 0)//生命为0，什么都不做
+			return;
+		Control();
+	}
+
+	void Control(){
+		//获取鼠标移动距离
+		float rh = Input.GetAxis("Mouse X");
+		float rv = Input.GetAxis("Mouse Y");
+		//旋转摄像机
+		m_camRot.x -= rv;
+		m_camRot.y += rh;
+		m_camTransform.eulerAngles = m_camRot;
+		//使主角的面向方向与摄像机一致
+		Vector3 camrot = m_camTransform.eulerAngles;
+		camrot.x = 0;
+		camrot.z = 0;
+		m_transform.eulerAngles = camrot;
+
+		float xm = 0, ym = 0, zm = 0;
+
+		//重力运动
+		ym -= m_gravity * Time.deltaTime;
+
+		//上下左右运动
+		if (Input.GetKey (KeyCode.W)) {
+			zm += m_movSpeed * Time.deltaTime;	
+		}
+		else if(Input.GetKey(KeyCode.S)){
+			zm -= m_movSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			xm -= m_movSpeed * Time.deltaTime;	
+		}
+		else if(Input.GetKey(KeyCode.D)){
+			xm += m_movSpeed * Time.deltaTime;
+		}
+
+		m_ch.Move (m_transform.TransformDirection (new Vector3 (xm, ym, zm)));
+
+		//使摄像机的位置与主角一致
+		Vector3 pos = m_transform.position;
+		pos.y += m_camHeight;
+		m_camTransform.position = pos;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawIcon (this.transform.position,"Spawn.tif");
+	}
+}
