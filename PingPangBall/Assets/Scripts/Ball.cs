e539264394fcdf7f5b18af9347bb3f8c8aebde71
WangWenBo <wangwenbomx@gmail.com>
2014-10-22 16:37:33 +0800

    Update

---
+++ PingPangBall/Assets/Scripts/Ball.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+	
+	public float m_speed = 5;
+	private int m_score = 0;
+	protected Transform m_transform;
+	private bool isStart = false;
+	private bool isFinish = false;
+	float move_x = 0, move_y = 0;
+
+	void Start ()
+	{
+		m_transform = this.transform;
+		move_x += m_speed * Time.deltaTime * Random.Range (1, 2);
+		move_y += m_speed * Time.deltaTime * Random.Range (1, 2);
+	}
+
+	void Update ()
+	{
+		GameObject obj = GameObject.FindGameObjectWithTag ("Player");
+		CubeMove start = obj.GetComponent<CubeMove> ();
+		if (start.startFlag == 1)
+			isStart = true;
+
+		if (isStart) {
+			this.m_transform.Translate (new Vector3 (move_x, move_y, 0));
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.skin.label.fontSize = 20;
+		//显示当前得分
+		GUI.Label(new Rect(5,1,Screen.width,30),"得分: "+m_score);
+
+		if (isFinish) {
+			//放大字体
+			GUI.skin.label.fontSize = 50;
+			//显示游戏失败
+			GUI.skin.label.alignment = TextAnchor.LowerCenter;
+			GUI.Label(new Rect(0,Screen.height * 0.2f,Screen.width,60),"游戏失败");
+			
+			GUI.skin.label.fontSize = 20;
+			//显示按钮
+			if(GUI.Button(new Rect(Screen.width * 0.5f-50,Screen.height * 0.5f,100,30),"再试一次"))
+			{
+				//读取当前关卡
+				Application.LoadLevel("1");
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag.CompareTo ("Player") == 0) {
+			move_y = -move_y;
+			move_x = move_x * Random.Range(0.5f,1.3f);
+			move_y = move_y * Random.Range(0.85f,1.2f);
+			m_score++;
+		}
+		if (other.tag.CompareTo ("Bound") == 0) {
+			move_x = -move_x;
+			move_y = move_y * Random.Range(0.85f,1.2f);
+		}
+		if (other.tag.CompareTo ("Finish") == 0) {
+			isFinish = true;
+		}
+	}
+}
---
+++ PingPangBall/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMove : MonoBehaviour {
+	
+	public float m_speed = 10;
+	protected Transform m_transform;
+	public int startFlag = 0;
+	float moveh = 0;
+
+	void Start () {
+		m_transform = this.transform;
+	}
+
+	void Update () {
+		moveh = 0;
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			if(startFlag == 0)
+				startFlag = 1;
+			moveh -= m_speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			if(startFlag == 0)
+				startFlag = 1;
+			moveh += m_speed * Time.deltaTime;
+		}
+		if(m_transform.position.x<-8 && moveh>0)
+			this.m_transform.Translate (new Vector3 (moveh, 0, 0));
+		if(m_transform.position.x>8 && moveh<0)
+			this.m_transform.Translate (new Vector3 (moveh, 0, 0));
+		if(m_transform.position.x>=-8 && m_transform.position.x<=8)
+			this.m_transform.Translate (new Vector3 (moveh, 0, 0));
+	}
+
+	void OnGUI()
+	{
+	}
+}
